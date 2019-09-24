4d183734dc0cf82e343fe51074e95f4fc3167781
Erick <erickcfuga@outlook.com>
2019-09-23 21:25:15 -0300

    Add command to generate a requests per service log

---
+++ app/Commands/RequestsPerConsumerCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+use App\Consumer;
+use App\Helpers\FileHandler;
+
+class RequestsPerConsumerCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'consumer:requests_count {output_file}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Shows the requests per consumer count';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+	$this->fileHandler = new FileHandler($this->argument('output_file'));
+	$this->fileHandler->append('consumer_id,requests_count');
+
+	foreach (Consumer::all() as $consumer) {
+	    $this->fileHandler->append(
+		[$consumer->id, $consumer->consumerRequests()->count()]
+	    );
+	}
+    }
+}
