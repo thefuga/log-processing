614fca58accfda1f3a00170f44bd0cdc378f14be
Erick <erickcfuga@outlook.com>
2019-09-23 21:23:14 -0300

    Add method to calculate average latency times

--- app/Service.php
+++ app/Service.php
@@ -15,4 +15,18 @@ class Service extends BaseModel
     {
         return $this->hasManyThrough(Request::class, Route::class);
     }
+
+    /**
+     * Returns the average latency time for a service.
+     * @param $latency is the type of the latency to be calculated (proxyLatency,
+     * kongLatency or requestLatency)
+     * @return average latency from all requests to this service.
+     */
+    public function averageLatencyTime($latency)
+    {
+        return $this->requests->map(function ($request) use ($latency) {
+            return $request->consumerRequest->$latency();
+        })->avg();
+    }
 }
+
